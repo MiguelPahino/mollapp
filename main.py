d227c8a77914a04e61e00761df66de6fcf283c28
MiguelPahino <mpahinogutierrez@gmail.com>
2026-02-28 17:36:09 +0100

    feat(main): :sparkles: Crear el main para el funcionamiento de la app

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from mollapp!")
+from src.clients.mollapp import Mollapp
 
+from src.administrador.programadorTasques import ProgramadorTasques
 
-if __name__ == "__main__":
-    main()
+from src.filtres.autontecicacion import Autenticacion
+from src.filtres.autorizacion import Autorizacion
+
+from src.targets.vehicle import Vehicle
+
+id = input("Dinos tu nombre: ")
+
+target = Vehicle()
+programadorTasques = ProgramadorTasques(target)
+
+programadorTasques.setTasca(Autenticacion())
+programadorTasques.setTasca(Autorizacion())
+
+mollapp = Mollapp()
+mollapp.setProgramadorTasques(programadorTasques)
+mollapp.enviar(id)
